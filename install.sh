03d0297c27c3b36ffa46de07c22db8209d062dbd
sxend <arimitsu.k+github@gmail.com>
2015-02-19 01:20:22 +0900

    add "zsh"files

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo apt-get -y install curl vim sublime-text
+sudo apt-get -y install curl vim sublime-text zsh
 
 git config --global alias.co checkout
 git config --global alias.st 'status'
 git config --global alias.br 'branch'
 
+curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install iojs
