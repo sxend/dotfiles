50ee00f4e5df4e8cad6f1744d7bf418684a9a4f4
sxend <arimitsu.k+github@gmail.com>
2015-03-07 12:14:27 +0900

    add sbt PATH

--- .zshrc
+++ .zshrc
@@ -1,2 +1,7 @@
+PATH_ARR+=($PATH)
 
 alias ll='ls -al'
+
+PATH_ARR+=("$HOME/.bin/sbt/bin")
+
+export PATH="$(IFS=:; echo "${PATH_ARR[*]}")"
--- install.sh
+++ install.sh
@@ -4,10 +4,15 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
+mkdir -p $HOME/.bin
+mkdir -p $HOME/downloads
+
 # install basic packages
 sudo apt-get -y install wget curl vim zsh git
 chsh -s `which zsh`
 cp .zshrc $HOME/.zshrc
+
+# !! reboot or re-login
 
 # git alias
 git config --global alias.co checkout
@@ -19,5 +24,10 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
 echo 'source $HOME/.nvm/nvm.sh' >> ~/.zshrc
 
 sudo apt-get -y install docker.io apparmor
+
+wget https://dl.bintray.com/sbt/native-packages/sbt/0.13.7/sbt-0.13.7.tgz
+tar xzf sbt-0.13.7.tgz
+mv sbt $HOME/.bin/sbt
+echo 'add PATH => $HOME/.bin/sbt/bin'
 
 
