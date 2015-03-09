9a5487dec16c8ba7d3da8f632e3b8f1475d07c15
sxend <arimitsu.k+github@gmail.com>
2015-03-09 18:22:30 +0900

    change version format

--- install.sh
+++ install.sh
@@ -52,24 +52,24 @@ local MVN_DIR=$HOME/usr/local/maven
 mkdir -p ${MVN_DIR}
 wget http://mirror.sdunix.com/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.zip
 unzip apache-maven-3.2.5-bin.zip
-mv apache-maven-3.2.5 ${MVN_DIR}/maven-3.2.5
-ln -s ${MVN_DIR}/maven-3.2.5/bin/mvn $HOME/bin/mvn
+mv apache-maven-3.2.5 ${MVN_DIR}/3.2.5
+ln -s ${MVN_DIR}/3.2.5/bin/mvn $HOME/bin/mvn
 rm apache-maven-3.2.5-bin.zip
 
 local SBT_DIR=$HOME/usr/local/sbt
 mkdir -p ${SBT_DIR}
 wget https://dl.bintray.com/sbt/native-packages/sbt/0.13.7/sbt-0.13.7.tgz
 tar xzf sbt-0.13.7.tgz
-mv sbt ${SBT_DIR}/sbt-0.13.7
-ln -s ${SBT_DIR}/sbt-0.13.7/bin/sbt $HOME/bin/sbt
+mv sbt ${SBT_DIR}/0.13.7
+ln -s ${SBT_DIR}/0.13.7/bin/sbt $HOME/bin/sbt
 rm sbt-0.13.7.tgz
 
 local GRADLE_DIR=$HOME/usr/local/gradle
 mkdir -p ${GRADLE_DIR}
 wget https://services.gradle.org/distributions/gradle-2.3-bin.zip
 unzip gradle-2.3-bin.zip
-mv gradle-2.3 ${GRADLE_DIR}/gradle-2.3
-ln -s ${GRADLE_DIR}/gradle-2.3/bin/gradle $HOME/bin/gradle
+mv gradle-2.3 ${GRADLE_DIR}/2.3
+ln -s ${GRADLE_DIR}/2.3/bin/gradle $HOME/bin/gradle
 rm gradle-2.3-bin.zip
 
 # Ricty install
